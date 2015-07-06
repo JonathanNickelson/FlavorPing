e78f3eef526576ae9f474bfb31e972ac0aba9634
JP Hochbaum <jphoc13@gmail.com>
2015-07-06 11:28:38 -0500

    Merchant and MenuItems classses added

---
+++ FlavorPing/Models/MenuItems.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlavorPing.Models
+{
+    public class MenuItems
+    {
+        public int ID { get; set; }
+
+        public string ItemName { get; set; }
+
+        public string Description { get; set; }
+
+        public virtual Merchant Merchant { get; set; }
+
+
+    }
+}
