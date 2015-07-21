18d88d4370d0c8fa6d1d63ee9df611886e8521ad
JP Hochbaum <jphoc13@gmail.com>
2015-07-21 16:41:15 -0500

    Added User Class but didn't scaffold yet.  Feel free to addd to this class any attributed I left out.

---
+++ FlavorPing/Models/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FlavorPing.Models
+{
+    public class User
+    {
+        
+        public int UserId { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string fName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string lName { get; set; }
+
+        [Required]
+        [Display(Name = "Zip Code")]
+        public int zip { get; set; }
+
+        [Required]
+        [Display(Name = "Phone #")]
+        public int phoneNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        public string email { get; set; }
+
+        public virtual List<Merchant> Merchant { get; set; }
+    }
+}
